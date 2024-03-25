b15ebf347cd4ba0c4f23aadf91aa62bcaeaaf63c
sushmita9026 <sushumita23@navgurukul.org>
2024-03-25 12:20:16 +0530

    first commit

---
+++ Arithmetic.js
@@ -0,0 +1,133 @@
+// sum,diffrence,prodect,quotient,modulus
+
+
+//let a =10;                                                                        //Q=1
+// let b =20;
+// let sum=a+b;
+// let diffrence = a-b;
+// let prodect =a*b;
+// let quotient =a/b;
+// let mode = a%b;
+// console.log(sum,diffrence,prodect,quotient,mode);
+
+
+
+// only print the integer
+
+// let a = 99;                                                                         //Q=2
+// let b = 5;
+//  console.log(parseInt(a/b));
+
+
+
+
+// let a = 3;                                                                         // Q=3
+// let b = 2;
+// let pow = a**b;
+// console.log(pow);
+
+     
+
+//only print the decimal part                                                           // Q = 4
+                                                           
+// let a=34;
+// let b=10;
+// let decimal = a%b;
+// let dpart= decimal/b;
+// console.log(dpart);
+
+
+//average of a 5 number
+
+// let a=67;                                                                                //Q = 5
+// let b= 77;
+// let c=99;
+// let d=88;
+// let e=78;
+// let average = (a+b+c+d+e)/5;
+// console.log(average);
+
+
+//profit
+
+// let cp =45;                                                                                // Q=6
+// let sp = 34;
+// let profit = cp-sp;
+// console.log(profit);
+
+
+//last digit of a number
+
+// let num =  345;                                                                              //  Q=7
+// let lastDigit = num %10;
+// console .log(lastDigit);
+
+
+
+//first and last digit
+
+// let number = 18;                                                                                 //Q=8
+// let firstDigit = parseInt(number / 10);
+// let lastDigit  = number % 10;
+// console.log("FirstDigit =",firstDigit,"\nLastDigit =",lastDigit); 
+ 
+
+
+//max of three number
+
+// let a =3;                                                                                          // Q=9
+// let b = 4;
+// let c =5;
+// if(a>b){
+//     if(a>c){
+//         console.log(a);
+//     } else{
+//         console.log(c);
+//     }
+// } else if(b>c){
+//     console.log(b);
+// } else{
+//     console.log(c);
+// }
+   
+
+
+ // even and odd
+
+// let num = 5;                                                                               //Q  =10
+// if(num%2==0){
+//     console.log("even");
+// } else{
+//     console.log("odd");
+// }
+
+
+
+   //prime Number
+   
+//    let n = 8;                                                                     // Q = 11
+//    let i=1;
+//    let c=0;
+//    while(i<=n){
+//     if(n%i==0){
+//         c=c+1;
+//     } i++;
+//    }
+//    if(c==2){
+//     console.log("prime number");
+//    } else{
+//     console.log("not prime");
+//    }
+
+
+
+
+// convert temperature from Celsius to Fahrenhei
+
+
+// let c =35;                                                                  // Q=12
+// let f =(c*9/5)+32;
+// console.log(f);
+//  f=95
+//  c = (f-32)*5/9;
+//  console.log(c);
